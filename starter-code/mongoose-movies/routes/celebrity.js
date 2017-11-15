2e8abc7f7b9cfdb6efaccc575cfb6d41370a256b
Alejandro Roman <aromanarguello@gmail.com>
2017-11-15 12:39:42 -0500

    start iteration 4

--- starter-code/mongoose-movies/routes/celebrity.js
+++ starter-code/mongoose-movies/routes/celebrity.js
@@ -18,20 +18,51 @@ router.get("/celebrities", (req, res, next) => {
 });
 
 router.get("/celebrities/:id", (req, res, next) => {
-  console.log(req.query.id);
+
   CelebrityModel
-  .findById(req.query.id)
-  .then( celebFromDb => {
-    res.locals.celebDetails = celebFromDb;
+  .findById(req.params.id)
+  .then( productFromDb => {
+
+    res.locals.celebrityDetails = productFromDb;
+    return CelebrityModel.find({ product: req.params.prodId }).exec();
+  })
+  .then( celebrityResults => {
+    res.locals.listOfCelebrities = celebrityResults;
     res.render("celebrities/show");
   })
-  .catch( err => {
+  .catch(err => {
+
     next(err);
   });
 });
 
-
-
-
+// ------------------------------
+// router.get("/products/:prodId", (req, res, next) => {
+//     //      /products/ 9999
+//     //                   |
+//     //      req.params.prodId
+//     // ProductModel.findOne({ _id: req.params.prodId })
+//     ProductModel.findById(req.params.prodId)
+//       .then((productFromDb) => {
+//           // create a local variable for the view to access the DB result
+//           res.locals.productDetails = productFromDb;
+//
+//           // now retrieve the reviews from the database
+//           // (return the promise of the next database operation)
+//           return ReviewModel.find({ product: req.params.prodId }).exec();
+//       })
+//       .then((reviewResults) => {
+//           res.locals.listOfReviews = reviewResults;
+//
+//           res.render("product-views/product-details");
+//       })
+//       .catch((err) => {
+//           // render the error page with our error
+//           next(err);
+//       });
+// }); // GET /products/:prodId
+//
+//
+//
 
 module.exports = router;
